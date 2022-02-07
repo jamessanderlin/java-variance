1ba19cd4e5a6c1dbc1b370d804cd3633a967f3cd
James Sanderlin <james.sanderlin@gmail.com>
2022-02-07 15:41:10 -0600

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // COVARIANCE
+
+        Fruit fruit = new Fruit();
+        Mango mango = new Mango();
+
+        fruit = mango;
+        // mango = fruit; // Doesn't work, assignment is covariant
+
+        Fruit[] fruitArray = {};
+        Mango[] mangoArray = {};
+
+        fruitArray = mangoArray;
+        // mangoArray = fruitArray; // Doesn't work, arrays are covariant 
+
+
+        // INVARIANCE
+
+        ArrayList<Fruit> fruitList = new ArrayList<Fruit>();
+        ArrayList<Mango> mangoList = new ArrayList<Mango>();
+
+        // fruitList = mangoList; // Doesn't work, generics are invariant
+        // mangoList = fruitList; // Doesn't work, generics are invariant
+    }
+}
+
+class Fruit {
+    // Sample Fruit class
+}
+
+class Mango extends Fruit {
+    // Sample Mango class (Subtype of Fruit)
+}
